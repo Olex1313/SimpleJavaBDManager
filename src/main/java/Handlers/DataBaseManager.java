2d159175595fb34cf9e022a2506b4ab80da2b591
Olex1313 <aalimonov_2@miem.hse.ru>
2021-07-29 08:44:36 +0300

    Add DB Handler methods && BD connection tests

--- src/main/java/Handlers/DataBaseManager.java
+++ src/main/java/Handlers/DataBaseManager.java
@@ -1,4 +1,37 @@
 package Handlers;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DataBaseManager {
+    private static Connection connection;
+
+    public DataBaseManager(String DB_URL, String USER, String PASS) {
+        System.out.println("PostgreSQL JDBC Driver successfully connected");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager
+                    .getConnection(DB_URL, USER, PASS);
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You successfully connected to database now");
+            this.connection = connection;
+        } else {
+            System.out.println("Failed to make connection to database");
+            this.connection = null;
+        }
+    }
+
+    public void getJSONTable(String table, String path) {
+
+    }
+
 }
---
+++ src/test/java/DBConnectionTest/JDBCPostgreSQLExample.java
@@ -0,0 +1,56 @@
+package DBConnectionTest;
+
+import java.sql.*;
+
+public class JDBCPostgreSQLExample {
+
+    //  Database credentials
+    static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/javatest";
+    static final String USER = "javauser";
+    static final String PASS = "javauser";
+
+    public static void main(String[] argv) {
+
+        System.out.println("Testing connection to PostgreSQL JDBC");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
+            e.printStackTrace();
+            return;
+        }
+
+        System.out.println("PostgreSQL JDBC Driver successfully connected");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager
+                    .getConnection(DB_URL, USER, PASS);
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed");
+            e.printStackTrace();
+            return;
+        }
+
+        if (connection != null) {
+            System.out.println("You successfully connected to database now");
+            try {
+                Statement st = connection.createStatement();
+                ResultSet rs = st.executeQuery("SELECT * FROM students");
+                while (rs.next()) {
+                    System.out.println(rs.getString(2));
+                }
+                rs.close();
+                st.close();
+                connection.close();
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Failed to make connection to database");
+        }
+    }
+}
